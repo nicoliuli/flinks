fbb7cb87732891d32e9153923b9d1e9f85d4312d
liuli <liuli@moqipobing.com>
2021-03-01 16:56:12 +0800

    feat: 监控练习

--- src/main/java/com/wb/day08/MetricDemo.java
+++ src/main/java/com/wb/day08/MetricDemo.java
@@ -1,8 +1,12 @@
 package com.wb.day08;
 
+import com.codahale.metrics.SlidingWindowReservoir;
 import org.apache.flink.api.common.functions.RichMapFunction;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.dropwizard.metrics.DropwizardHistogramWrapper;
+import org.apache.flink.dropwizard.metrics.DropwizardMeterWrapper;
 import org.apache.flink.metrics.Counter;
+import org.apache.flink.metrics.Gauge;
 import org.apache.flink.metrics.Histogram;
 import org.apache.flink.metrics.HistogramStatistics;
 import org.apache.flink.streaming.api.TimeCharacteristic;
@@ -26,13 +30,39 @@ public class MetricDemo {
 
         SingleOutputStreamOperator<String> stream = dataStreamSource.map(new RichMapFunction<String, String>() {
             private Counter counter;
+            private transient int valueToExpose = 0;
+            private transient Histogram histogram;
+            private transient DropwizardMeterWrapper meter;
+
             @Override
             public void open(Configuration parameters) throws Exception {
                 this.counter = getRuntimeContext().getMetricGroup().counter("myCounter",new MyCounter());
+                getRuntimeContext().getMetricGroup().gauge("myGuage", new Gauge<Integer>() {
+                    @Override
+                    public Integer getValue() {
+                        return valueToExpose;
+                    }
+                });
+
+                com.codahale.metrics.Histogram dropwizardHistogram =
+                        new com.codahale.metrics.Histogram(new SlidingWindowReservoir(500));
+                this.histogram = getRuntimeContext()
+                        .getMetricGroup()
+                        .histogram("myHistogram", new DropwizardHistogramWrapper(dropwizardHistogram));
+
+                com.codahale.metrics.Meter dropwizardMeter = new com.codahale.metrics.Meter();
+
+                this.meter = getRuntimeContext()
+                        .getMetricGroup()
+                        .meter("myMeter", new DropwizardMeterWrapper(dropwizardMeter));
             }
 
             @Override
             public String map(String value) throws Exception {
+                counter.inc();
+                valueToExpose++;
+                histogram.update(valueToExpose);
+                this.meter.markEvent();
                 return value;
             }
         });
